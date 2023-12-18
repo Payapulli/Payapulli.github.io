cd00d345db93f1a06cd9b56663029cc48c9b0816
Joshua Payapulli <joshua.payapulli@gmail.com>
2023-12-18 01:26:30 -0800

    sdfsdf

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "",
+        "excerpt":"I am a first year Master’s Student studying Applied Data Science at The University of Southern California.  ","categories": [],
+        "tags": [],
+        "url": "/aboutme/",
+        "teaser": null
+      }]
